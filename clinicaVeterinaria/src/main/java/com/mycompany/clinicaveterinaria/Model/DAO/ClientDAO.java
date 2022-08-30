a6ec22f27c46661c03964998b45b6af923ce0ef6
Guilherme Trevisan <guilherme.hct@hotmail.com>
2022-08-29 22:31:07 -0300

    move some classes and create ClientDAO

---
+++ clinicaVeterinaria/src/main/java/com/mycompany/clinicaveterinaria/Model/DAO/ClientDAO.java
@@ -0,0 +1,54 @@
+package com.mycompany.clinicaveterinaria.Model.DAO;
+import com.mycompany.clinicaveterinaria.Model.Client;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClientDAO {
+    private List<Client> users = new ArrayList<>();
+
+    public ClientDAO() {}
+
+    public List<Client> getAllUsers() {
+        return users;
+    }
+    
+    public Client getUserById(int id) { 
+        for(int i = 0; i < users.size(); i++) {
+        var user = users.get(i);
+         if(user.getId() == id) {
+            return user;
+        }
+      }
+        return null;
+    }
+    
+    public void insertNewClient(String name, String address, String number, long cep, String email) {
+        var newClient = new Client(name, address, number, cep, email);
+        users.add(newClient);
+    }
+    
+    public void updateClientById(int id, String name, String address, String number, long cep, String email) {
+        for(int i = 0; i < users.size(); i++) {
+         var user = users.get(i);
+         if(user.getId() == id) {
+            user.setName(name);
+            user.setAddress(address);
+            user.setNumber(number);
+            user.setCep(cep);
+            user.setEmail(email);
+        }
+      }
+    }
+    
+    public void deleteClientById(int id) {
+        List<Client> newUsers = new ArrayList<>();
+        
+        for(int i = 0; i < users.size(); i++) {
+            var user = users.get(i);
+            if(user.getId() != id) {
+                newUsers.add(user);
+        }
+      }
+        users = newUsers;
+    }
+}
