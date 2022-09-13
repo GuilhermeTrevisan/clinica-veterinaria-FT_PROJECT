0ce0bf0169b62b2bccf116e76be42e4d6c780b67
Guilherme Trevisan <43553759+GuilhermeTrevisan@users.noreply.github.com>
2022-09-12 22:11:48 -0300

    create DAO and update ClientDAO

--- clinicaVeterinaria/src/main/java/com/mycompany/clinicaveterinaria/Model/DAO/ClientDAO.java
+++ clinicaVeterinaria/src/main/java/com/mycompany/clinicaveterinaria/Model/DAO/ClientDAO.java
@@ -5,52 +5,88 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class ClientDAO {
-    private List<Client> users = new ArrayList<>();
+public class ClientDAO extends DAO {
 
-    public ClientDAO() {}
+    public ClientDAO() {
+        getConnection();
+        createTable();
+    }
 
     public List<Client> getAllUsers() {
-        return users;
+        List<Cliente> clientes = new ArrayList();
+        ResultSet rs = getResultSet(query);
+        try {
+            while (rs.next()) {
+                clientes.add(buildObject(rs));
+            }
+        } catch (SQLException e) {
+            System.err.println("Exception: " + e.getMessage());
+        }
+        return clientes;
     }
     
-    public Client getUserById(int id) { 
-        for(int i = 0; i < users.size(); i++) {
-        var user = users.get(i);
-         if(user.getId() == id) {
-            return user;
+    public Client getUserById(int id) {
+        String query = "SELECT * FROM client WHERE id = " + id
+        ResultSet rs = getResultSet(query);
+
+        Cliente cliente = null;
+        try {
+            cliente = new Cliente(rs.getString("name"), rs.getString("address"), rs.getString("number"), rs.getString("cep"), rs.getString("email"));
+        } catch (SQLException e) {
+            System.err.println("Exception: " + e.getMessage());
         }
-      }
-        return null;
+        return cliente;
     }
     
     public void insertNewClient(String name, String address, String number, long cep, String email) {
-        var newClient = new Client(name, address, number, cep, email);
-        users.add(newClient);
+        try {
+            PreparedStatement stmt;
+            stmt = DAO.getConnection().prepareStatement("INSERT INTO client (name, address, number, cep, email) VALUES (?,?,?,?,?)");
+            stmt.setString(1, name);
+            stmt.setString(2, address);
+            stmt.setString(3, number);
+            stmt.setString(4, cep);
+            stmt.setString(5, email);
+            executeUpdate(stmt);
+        } catch (SQLException ex) {
+            Logger.getLogger(ClienteDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     public void updateClientById(int id, String name, String address, String number, long cep, String email) {
-        for(int i = 0; i < users.size(); i++) {
-         var user = users.get(i);
-         if(user.getId() == id) {
-            user.setName(name);
-            user.setAddress(address);
-            user.setNumber(number);
-            user.setCep(cep);
-            user.setEmail(email);
+        try {
+            PreparedStatement stmt;
+            stmt = DAO.getConnection().prepareStatement("UPDATE cliente SET name=?, addres=?, number=?, cep=?, email=? WHERE id=?");
+            stmt.setString(1, name);
+            stmt.setString(2, addres);
+            stmt.setString(3, number);
+            stmt.setString(4, cep);
+            stmt.setString(5, email);
+            stmt.setInt(6, id);
+            executeUpdate(stmt);
+        } catch (SQLException e) {
+            System.err.println("Exception: " + e.getMessage());
         }
-      }
     }
     
     public void deleteClientById(int id) {
-        List<Client> newUsers = new ArrayList<>();
-        
-        for(int i = 0; i < users.size(); i++) {
-            var user = users.get(i);
-            if(user.getId() != id) {
-                newUsers.add(user);
+        PreparedStatement stmt;
+        try {
+            stmt = DAO.getConnection().prepareStatement("DELETE FROM cliente WHERE id = ?");
+            stmt.setInt(1, id);
+            executeUpdate(stmt);
+        } catch (SQLException e) {
+            System.err.println("Exception: " + e.getMessage());
         }
-      }
-        users = newUsers;
+    }
+
+    private Cliente buildObject(ResultSet rs) {
+        Cliente cliente = null;
+        try {
+            cliente = new Cliente(rs.getString("name"), rs.getString("address"), rs.getString("number"), rs.getString("cep"), rs.getString("email"));
+        } catch (SQLException e) {
+            System.err.println("Exception: " + e.getMessage());
+        }
+        return cliente;
     }
 }
---
+++ clinicaVeterinaria/src/main/java/com/mycompany/clinicaveterinaria/Model/DAO/DAO.java
@@ -0,0 +1,131 @@
+
+import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public abstract class DAO {
+    public static final String DBURL = "jdbc:sqlite:vet2022.db";
+    private static Connection con;
+    protected static SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+
+    public static Connection getConnection() {
+        if (con == null) {
+            try {
+                con = DriverManager.getConnection(DBURL);
+                if (con != null) {
+                    DatabaseMetaData meta = con.getMetaData();
+                }
+            } catch (SQLException e) {
+                System.err.println("Exception: " + e.getMessage());
+            }
+        }
+        return con;
+    }
+
+    protected ResultSet getResultSet(String query) {
+        Statement s;
+        ResultSet rs = null;
+        try {
+            s = (Statement) con.createStatement();
+            rs = s.executeQuery(query);
+        } catch (SQLException e) {
+            System.err.println("Exception: " + e.getMessage());
+        }
+        return rs;
+    }
+
+    protected int executeUpdate(PreparedStatement queryStatement) throws SQLException {
+        int update;
+        update = queryStatement.executeUpdate();
+        return update;
+    }
+
+    protected int lastId(String tableName, String primaryKey) {
+        Statement s;
+        int lastId = -1;
+        try {
+            s = (Statement) con.createStatement();
+            ResultSet rs = s.executeQuery("SELECT MAX(" + primaryKey + ") AS id FROM " + tableName);
+            if (rs.next()) {
+                lastId = rs.getInt("id");
+            }
+        } catch (SQLException e) {
+            System.err.println("Exception: " + e.getMessage());
+        }
+        return lastId;
+    }
+
+    public static void closeConnection() {
+        try {
+            (DAO.getConnection()).close();
+        } catch (SQLException e) {
+            System.err.println("Exception: " + e.getMessage());
+        }
+    }
+
+    protected final boolean createAllTables() {
+        try {
+            PreparedStatement stmt;
+            // Client:
+            stmt = DAO.getConnection().prepareStatement("CREATE TABLE IF NOT EXISTS client( \n"
+                    + "id INTEGER PRIMARY KEY, \n"
+                    + "name VARCHAR, \n"
+                    + "address VARCHAR, \n"
+                    + "cep VARCHAR, \n"
+                    + "email VARCHAR, \n"
+                    + "number VARCHAR); \n");
+            executeUpdate(stmt);
+            // Animal:
+            stmt = DAO.getConnection().prepareStatement("CREATE TABLE IF NOT EXISTS animal( \n"
+                    + "id INTEGER PRIMARY KEY, \n"
+                    + "name VARCHAR, \n"
+                    + "genre VARCHAR, \n"
+                    + "id_especie INTEGER, \n"
+                    + "id_cliente INTEGER); \n");
+            executeUpdate(stmt);
+            // Species:
+            stmt = DAO.getConnection().prepareStatement("CREATE TABLE IF NOT EXISTS species( \n"
+                    + "id INTEGER PRIMARY KEY, \n"
+                    + "name VARCHAR); \n");
+            executeUpdate(stmt);
+            // Veterinary:
+            stmt = DAO.getConnection().prepareStatement("CREATE TABLE IF NOT EXISTS veterinary( \n"
+                    + "id INTEGER PRIMARY KEY, \n"
+                    + "name VARCHAR, \n"
+                    + "address VARCHAR, \n"
+                    + "number VARCHAR); \n");
+            executeUpdate(stmt);        
+            // Treatment:
+            stmt = DAO.getConnection().prepareStatement("CREATE TABLE IF NOT EXISTS treatment( \n"
+                    + "id INTEGER PRIMARY KEY, \n"
+                    + "id_animal INTEGER, \n"
+                    + "initDate TEXT, \n"
+                    + "finishDate TEXT, \n");
+            executeUpdate(stmt);
+            // Appointment:
+            stmt = DAO.getConnection().prepareStatement("CREATE TABLE IF NOT EXISTS appointment( \n"
+                    + "id INTEGER PRIMARY KEY, \n"
+                    + "date TEXT, \n"
+                    + "historic VARCHAR, \n"
+                    + "id_animal INTEGER, \n"
+                    + "id_vet INTEGER, \n"
+                    + "id_tratamento INTEGER, \n");
+            executeUpdate(stmt);            
+             // Exam:
+            stmt = DAO.getConnection().prepareStatement("CREATE TABLE IF NOT EXISTS exam( \n"
+                    + "id INTEGER PRIMARY KEY, \n"
+                    + "description VARCHAR, \n"
+                    + "id_consulta INTEGER); \n");
+            executeUpdate(stmt);      
+            // Default element for species:
+            stmt = DAO.getConnection().prepareStatement("INSERT OR IGNORE INTO especie (id, nome) VALUES (1, 'Cachorro')");
+            executeUpdate(stmt);
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(DAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+
+}
